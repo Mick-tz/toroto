f9e59cf3caaaaadfb11c40e14f400f4a6f16e864
cupc4ke <miguel.martinez@ciencias.unam.mx>
2021-02-22 18:36:14 -0600

    filtro de proyectos por estado

--- toroto/src/store/proyectos/getters.js
+++ toroto/src/store/proyectos/getters.js
@@ -2,6 +2,13 @@ export function getProyectos (state) {
     return state.proyectos
   }
   
+  export const getProyectosPorEstado = (state) => (estado) => {
+    console.log(estado)
+    return state.proyectos.filter(proyecto => {
+      return proyecto.location.toLowerCase().startsWith(estado.toLowerCase())
+    })
+  }
+
   export function getStatusCargandoProyectos (state) {
     return state.cargandoProyectos
   }
